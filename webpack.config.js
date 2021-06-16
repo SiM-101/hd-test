83756f67fb69dfe576dc796ebfd7466963331b47
Simone <86007260+SiM-101@users.noreply.github.com>
2021-06-16 15:52:24 +0100

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env) => {
+	const outFolder = env.mode === "development" ? "dist" : "docs";
+	const rootPath = path.resolve(__dirname);
+	const distPath = path.resolve(rootPath, outFolder);
+
+	return {
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"],
+		},
+		entry: [path.resolve(rootPath, "src", "index.tsx")],
+		devtool: env.mode === "development" ? "source-map" : false,
+		mode: env.mode,
+		module: {
+			rules: [
+				{
+					test: /\.ts(x?)$/,
+					exclude: /node_modules/,
+					use: [{ loader: "ts-loader" }],
+				},
+			],
+		},
+		output: {
+			path: distPath,
+			publicPath: "/",
+			filename: "[name].js",
+		},
+		devServer: {
+			contentBase: distPath,
+			liveReload: true,
+			port: 9000,
+			historyApiFallback: true,
+			writeToDisk: true,
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: path.resolve(rootPath, "index.html"),
+			}),
+		],
+	};
+};
