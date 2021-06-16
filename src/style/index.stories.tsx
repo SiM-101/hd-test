df38528b5a3756b87636497df88c6c3c9226e247
Simone <86007260+SiM-101@users.noreply.github.com>
2021-06-16 19:43:41 +0100

    Link and CTA

--- src/style/index.stories.tsx
+++ src/style/index.stories.tsx
@@ -13,9 +13,16 @@ export const Headers = () => (
 		<h5>Hastings Direct</h5>
 	</div>
 );
+
 export const Container = () => <div className={"container"}>Hello</div>;
 
-export const Link = () => <a href={"#"}>A</a>;
+export const Link = () => <a className={"link"} href={"#"}>Hello this is a link</a>;
+Link.parameters = { layout: "centered" };
 
 export const Card = () => <div className={"card"}>Hello card</div>;
 Card.parameters = { layout: "centered" };
+
+export const CTA = () => <button className={"cta"}>Hello CTA</button>;
+CTA.parameters = { layout: "centered" };
+
+export const CTAFill = () => <button className={"cta cta--fill"}>Hello CTA</button>;
